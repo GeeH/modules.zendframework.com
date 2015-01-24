7e4b8e39cc3b255a04b0ab5082cc8c6329fe609e
Gary Hockin <gary@hock.in>
2015-01-24 14:44:14 +0100

    Basic acceptance testing with Codeception

---
+++ tests/acceptance/HomeCept.php
@@ -0,0 +1,7 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('View the home page');
+$I->amOnPage('/');
+$I->see('Welcome to the ZF2 Modules Site');
+$I->click('Zf2Acl');
+$I->see('Zf2Acl');
